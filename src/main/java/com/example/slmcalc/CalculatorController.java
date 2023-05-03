d0030295b0dfee656312ca9018626b5f5ec99a1b
Masenku <marcel.ivanic@gmx.at>
2023-05-03 11:08:08 +0200

    add sum method comment

--- src/main/java/com/example/slmcalc/CalculatorController.java
+++ src/main/java/com/example/slmcalc/CalculatorController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
+
+    /**
+     * add two integers
+     */
     @RequestMapping("/calc/sum")
     public int sum(@RequestParam int a,
                    @RequestParam int b
